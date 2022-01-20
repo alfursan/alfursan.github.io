ef1cab1dfa19f914b393a8d27be3905041863205
Ahmed Thahir <94914628+AhmedThahir@users.noreply.github.com>
2022-01-20 12:15:09 +0400

    Add files via upload

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,1 +1,3 @@
-console.log("test");
+var assets = document.querySelectorAll("img, video, audio, iframe");
+for (var i = 0; i < assets.length; i++)
+    assets[i].setAttribute("loading", "lazy");
--- Website/js/main.ts
+++ Website/js/main.ts
@@ -1,1 +1,3 @@
-console.log("test")
+const assets = document.querySelectorAll("img, video, audio, iframe")
+for (let i = 0; i < assets.length; i++)
+  assets[i].setAttribute("loading", "lazy")
