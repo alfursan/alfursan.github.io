8b47b070732a6920db27a869fd7ce759b1894854
Ahmed Thahir <ahmedthahir2002@gmail.com>
2022-02-27 12:11:00 +0400

    r

--- js/lazyLoad.ts
+++ js/lazyLoad.ts
@@ -1,38 +1,32 @@
-const bgColor:string = "hsla(0, 0%, 0%, 0.75)"
+const bgColor:string = "hsla(0, 0%, 0%, 0.65)"
 const lazyLoadOptions = {
   threshold: 0,
   rootMargin: "0px 0px 300px 0px"
 };
 
 // assets
-const assets = document.querySelectorAll("img, video, audio, iframe")
-for (let i = 0; i < assets.length; i++)
-{
-  // assets[i].setAttribute("src", "assets/loading.svg")
-  assets[i].setAttribute("loading", "lazy")
-}
+const assets = document.querySelectorAll("[data]")
 const assetObserver = new IntersectionObserver((entries, assetObserver) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) return
 
-    const asset = <HTMLImageElement> entry.target
+    const asset = <HTMLMediaElement> entry.target
     asset.src = "assets/" + asset.getAttribute("data")
-    asset.classList.add("loaded")
     assetObserver.unobserve(entry.target)
   });
 }, lazyLoadOptions)
 assets.forEach((asset) => {
-  assetObserver.observe(asset)
+  asset.setAttribute("loading", "lazy")
+  assetObserver.observe(asset);
+
+  (<HTMLMediaElement> asset).onload = function (){
+    asset.classList.add("loaded")
+  }
 })
 
 // bg
 const gradient:string = "linear-gradient(" + bgColor + ", " + bgColor + "), url(assets/"
 const backgrounds = document.querySelectorAll("[bg]")
-// for (let i = 0; i < backgrounds.length; i++) {
-//   const background = <HTMLElement>backgrounds[i];
-//   const url = "loading.svg"
-//   background.style.backgroundImage = gradient + url + ")"
-// }
 
 const bgObserver = new IntersectionObserver((entries, bgObserver) => {
   entries.forEach((entry) => {
