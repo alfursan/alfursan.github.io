04e28818bcd3fc970eb0e194298236784c865656
Ahmed Thahir <ahmedthahir2002@gmail.com>
2022-04-15 09:35:24 +0400

    r

--- js/lazyLoad.ts
+++ js/lazyLoad.ts
@@ -1,5 +1,6 @@
 const assetLoc = "../" + "assets/"
 
+const black:string = "hsla(0, 0%, 5%, 1.00)"
 const blackt:string = "hsla(0, 0%, 0%, 0.90)"
 const bgColor:string = "hsla(0, 0%, 0%, 0.65)"
 const lazyLoadOptions = {
@@ -29,7 +30,8 @@ assets.forEach((asset) => {
 
 // bg
 // const gradient:string = "linear-gradient(" + bgColor + ", " + bgColor + "), url(" + assetLoc
-const gradient:string = "linear-gradient(" + blackt + " 0%, " + bgColor + " 15%, " + bgColor + " 85%, "+ blackt + " 100%), url(" + assetLoc
+// const gradient:string = "linear-gradient(" + blackt + " 0%, " + bgColor + " 15%, " + bgColor + " 85%, "+ blackt + " 100%), url(" + assetLoc
+const gradient:string = "linear-gradient(" + black + " 0%, " + bgColor + " 20%, " + bgColor + " 80%, "+ black + " 100%), url(" + assetLoc
 
 const backgrounds = document.querySelectorAll("[bg]")
 const bgObserver = new IntersectionObserver((entries, bgObserver) => {
